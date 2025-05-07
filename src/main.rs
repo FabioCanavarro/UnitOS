bfb55cf556a22b71cf810ab313b5b6e87f65b614
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-05-07 19:19:07 +0700

    Initial commit, holy shit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// cargo rustc -- -C link-args="/ENTRY:_start /SUBSYSTEM:console"
+
+
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic (_info : &PanicInfo) -> ! {
+    loop {}
 }
