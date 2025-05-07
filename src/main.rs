ffaf7a0c995b88593f103e0229c78ae09b10f825
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-05-07 22:36:11 +0700

    Vga buffer??

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{isize, panic::PanicInfo};
+
+static HELLO: &[u8] = b"Hello World";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    
+    /*
+    * NOTE: From my understanding
+    * the index starts from 0 so 
+    * the 0xb8000 should be the first u8 of Hello
+    * 0 * 2 + 1 = 1, to put the color code infront
+    * then continue on from there
+    */
+    for (i,&byte) in HELLO.iter().enumerate(){
+        let step: isize = i as isize * 2;
+        unsafe {
+            *vga_buffer.offset(step) = byte;
+            *vga_buffer.offset(step + 1) = 0xb;
+
+        };
+    }
+
     loop {}
 }
 
