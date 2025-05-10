cf45afa6d1bd2be94d4e744406e5950fb6e1e085
Fabio Canavarro <128808537+FabioCanavarro@users.noreply.github.com>
2025-05-10 15:55:03 +0700

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,8 @@ set -e
 export CARGO_MANIFEST_DIR=$(pwd)
 cargo bootimage --target x86_64-unit_os.json
 
-QEMU_BIN="${{ steps.qemu-setup.outputs.qemu-system-x86_64 }}"
 if [ -z "$QEMU_BIN" ]; then
-  echo "Error: QEMU executable path not found."
+  echo "Error: QEMU_BIN environment variable not set."
   exit 1
 fi
 
