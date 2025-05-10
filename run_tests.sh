183351819784b0d4de97a7d7e5333883b8f1111c
Fabio Canavarro <128808537+FabioCanavarro@users.noreply.github.com>
2025-05-10 15:53:04 +0700

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
+export CARGO_MANIFEST_DIR=$(pwd)
 cargo bootimage --target x86_64-unit_os.json
 
-qemu-system-x86_64 \
-    -drive format=raw,file=target/x86_64-unit_os/debug/bootimage-rusty_os.bin \
-    -no-reboot \
-    -device isa-debug-exit,iobase=0xf4,iosize=0x04 \
-    -serial stdio \
-    -display none
+QEMU_BIN="${{ steps.qemu-setup.outputs.qemu-system-x86_64 }}"
+if [ -z "$QEMU_BIN" ]; then
+  echo "Error: QEMU executable path not found."
+  exit 1
+fi
+
+"$QEMU_BIN" \
+  -drive format=raw,file=target/x86_64-unit_os/debug/bootimage-rusty_os.bin \
+  -no-reboot \
+  -device isa-debug-exit,iobase=0xf4,iosize=0x04 \
+  -serial stdio \
+  -display none
