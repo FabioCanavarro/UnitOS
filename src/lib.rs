bcf9a6b80558c06e2476e656f33b82acd28724ff
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-05-10 23:14:57 +0700

    Added lib.rs and the content, and intergrated testing folder and an test template

---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+#![no_std]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![allow(unused_imports)]
+
+pub mod serial;
+pub mod vga;
+pub mod test_trait;
+
+use test_trait::Tests;
+use core::panic::PanicInfo;
+use x86_64::instructions::port::Port;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    /*
+        * NOTE: From my understanding
+        * the index starts from 0 so
+        * the 0xb8000 should be the first u8 of Hello
+        * 0 * 2 + 1 = 1, to put the color code infront
+        * then continue on from there
+
+        for (i,&byte) in HELLO.iter().enumerate(){
+            let step: isize = i as isize * 2;
+            unsafe {
+                *vga_buffer.offset(step) = byte;
+                *vga_buffer.offset(step + 1) = 0xb;
+
+            };
+        }
+    */
+
+    println!("UnitOs");
+
+    // Running all tests
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+pub fn panic(info: &PanicInfo) -> ! {
+    println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+pub fn test_runner(tests: &[&dyn Tests]) {
+    serial_println!("Running {} tests\n", tests.len());
+    for test in tests {
+        test.run();
+    }
+    serial_print!("\n");
+    serial_println!("Exit Code: 1");
+    serial_println!("Success\n");
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success,
+    Failed,
+}
+
+impl From<QemuExitCode> for u32 {
+    fn from(value: QemuExitCode) -> Self {
+        match value {
+            QemuExitCode::Success => 0x10,
+            QemuExitCode::Failed => 0x11,
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
