d51af3b4d478b2d6508ac54ebb5170dc57d44a30
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-05-13 22:06:49 +0700

    Basic breakpoint interrupt

--- src/handler/interrupt_table.rs
+++ src/handler/interrupt_table.rs
@@ -1,31 +1,11 @@
-use x86_64::structures::idt::{
-    Entry,
-    HandlerFunc,
-    HandlerFuncWithErrCode,
-    PageFaultHandlerFunc
-};
+#![feature(abi_x86_interrupt)]
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use crate::println;
 
-#[repr(C)]
-pub struct InterruptDescriptorTable {
-    pub divide_by_zero: Entry<HandlerFunc>,
-    pub debug: Entry<HandlerFunc>,
-    pub non_maskable_interrupt: Entry<HandlerFunc>,
-    pub breakpoint: Entry<HandlerFunc>,
-    pub overflow: Entry<HandlerFunc>,
-    pub bound_range_exceeded: Entry<HandlerFunc>,
-    pub invalid_opcode: Entry<HandlerFunc>,
-    pub device_not_available: Entry<HandlerFunc>,
-    pub double_fault: Entry<HandlerFuncWithErrCode>,
-    pub invalid_tss: Entry<HandlerFuncWithErrCode>,
-    pub segment_not_present: Entry<HandlerFuncWithErrCode>,
-    pub stack_segment_fault: Entry<HandlerFuncWithErrCode>,
-    pub general_protection_fault: Entry<HandlerFuncWithErrCode>,
-    pub page_fault: Entry<PageFaultHandlerFunc>,
-    pub x87_floating_point: Entry<HandlerFunc>,
-    pub alignment_check: Entry<HandlerFuncWithErrCode>,
-    pub machine_check: Entry<HandlerFunc>,
-    pub simd_floating_point: Entry<HandlerFunc>,
-    pub virtualization: Entry<HandlerFunc>,
-    pub security_exception: Entry<HandlerFuncWithErrCode>,
-    // some fields omitted
+
+static mut INTERRUPT_TABLE : InterruptDescriptorTable = InterruptDescriptorTable::new();
+
+extern "x86-interrupt"
+fn breakpoint(stack_frame: InterruptStackFrame){
+    println!("EXCEPTION: BREAKPOINT\n{:?}",stack_frame)
 }
