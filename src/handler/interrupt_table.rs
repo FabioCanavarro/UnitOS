480ab954e35c9135686cdbff66d5cc672c93852a
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-05-13 22:32:48 +0700

    Changed test location

--- src/handler/interrupt_table.rs
+++ src/handler/interrupt_table.rs
@@ -23,8 +23,3 @@ fn breakpoint_handler(stack_frame: InterruptStackFrame){
     println!("EXCEPTION: BREAKPOINT\n{:?}",stack_frame)
 }
 
-#[test_case]
-fn test_breakpoint_exception() {
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
-}
