369b8b35761b5e09a4d05bdea01daafa37afa2b9
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-05-14 21:50:26 +0700

    Double fault handler

--- src/handler/interrupt_table.rs
+++ src/handler/interrupt_table.rs
@@ -9,15 +9,27 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt: InterruptDescriptorTable = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
+
 
 pub fn init_idt() {
     IDT.load();
 }
 
 // Handler Funcs
-extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+extern "x86-interrupt"
+fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:?}", stack_frame)
+}
+
+extern "x86-interrupt"
+fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64
+) -> ! {
+    println!("EXCEPTION: DOUBLE FAULT\n{:#?}\n error_code: \t{:#?}", stack_frame,error_code);
+    panic!("");
 }
