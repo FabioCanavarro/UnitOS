a95cb864841eaaabc2dfee2ef10debda6240b391
Fabio Canavarro <128808537+FabioCanavarro@users.noreply.github.com>
2025-05-16 16:38:03 +0700

    Update interrupt_table.rs

--- src/handler/interrupt_table.rs
+++ src/handler/interrupt_table.rs
@@ -10,8 +10,8 @@ lazy_static! {
         let mut idt: InterruptDescriptorTable = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
         idt
-    }
-};
+    };
+}
 
 pub fn init_idt() {
     IDT.load();
