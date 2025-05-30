611cb5f78983a5c1395e9256db619f03bd824e29
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-05-30 23:22:24 +0700

    Fixed Cargo fmt

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -32,7 +32,7 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-lazy_static! {
+lazy_static! (
     static ref TEST_IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         unsafe {
@@ -42,7 +42,7 @@ lazy_static! {
         }
         idt
     };
-};
+);
 
 pub extern "x86-interrupt" fn test_double_fault_handler(
     _stack_frame: InterruptStackFrame,
